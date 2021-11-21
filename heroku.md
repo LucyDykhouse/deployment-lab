866af9d1b9b6379bc23962d4841de650b30db7c8
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-21 12:24:27 -0500

    Added header and image

---
+++ heroku.md
@@ -0,0 +1,2 @@
+## Heroku link is below
+https://deployment-demo-1.herokuapp.com/
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
 const path = require('path');
 
 const app = express();
 
+// Endpoints
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../main.js'));
+});
+
+app.get("/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.css"));
 });
 
 
